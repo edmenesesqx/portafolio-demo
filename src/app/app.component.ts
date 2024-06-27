0221c0ed16093288dad5f9464a0e1a262fb12d83
Edgar Antonio Meneses Cadena <edgar.meneses@Quipux.local>
2024-06-26 22:32:57 -0500

    removed unused

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CardComponent } from './componets/card/card.component';
-import { PortafolioComponent } from './componets/portafolio/portafolio.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CardComponent,PortafolioComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,7 @@
 import { Routes } from '@angular/router';
-import { CardComponent } from './componets/card/card.component';
-import { PortafolioComponent } from './componets/portafolio/portafolio.component';
-import { ClienteComponent } from './componets/cliente/cliente.component';
 import { PlataformaServicioComponent } from './componets/plataforma-servicio/plataforma-servicio.component';
 
 export const routes: Routes = [
-    { path: 'card' , component: CardComponent},
-    { path: 'portafolio' , component: PortafolioComponent},
-    { path: 'cliente' , component: ClienteComponent},
     { path: 'plataforma-servicio', component: PlataformaServicioComponent},
-    { path: '' , redirectTo: '/card' ,pathMatch: 'full'}
+    { path: '' , redirectTo: '/plataforma-servicio' ,pathMatch: 'full'}
 ];
